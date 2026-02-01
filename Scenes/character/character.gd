fcc74cc40b7168c060affeac40d5004c98ab5aa7
Christopher Pina <49688431+Christopherpina2022@users.noreply.github.com>
2026-02-01 10:34:15 -0600

    Added new theme to Game UI

--- Scenes/character/character.gd
+++ Scenes/character/character.gd
@@ -3,6 +3,3 @@ extends Sprite2D
 func assignSprite(name: String) -> void:
 	# Assign Texture
 	texture = RoomManager.characterLibrary[name]
-
-func _ready() -> void:
-	assignSprite("robert")
--- Scenes/menu/menu.gd
+++ Scenes/menu/menu.gd
@@ -1,14 +1,10 @@
 extends Control
 
 func _on_exit_pressed() -> void:
-	# close the game
 	get_tree().quit()
 
-
 func _on_start_pressed() -> void:
-	# for now change scene to Waiting room
 	get_tree().change_scene_to_file("res://Scenes/room/RoomTemplate.tscn")
-
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/credits/Credits.tscn")
